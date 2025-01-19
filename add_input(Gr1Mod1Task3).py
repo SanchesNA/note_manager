2b8d96897ced66806b63617f6018db1a8707f75d
SanchesNA <shura.novozhilov.02@mail.ru>
2025-01-19 12:15:27 +1000

    Add files via upload

---
+++ add_input(Gr1Mod1Task3).py
@@ -0,0 +1,16 @@
+# Ввод переменных при помощи input
+
+username = input("Введите Ваше имя ")
+title = input("Введите заголовок заметки")
+status = input("Задайте статус заметки в формате done/undone ")
+content = input("Введите содержание заметки ")
+created_date = input("Введите дату создания заметки в формате дд-мм-гггг ")
+issue_date = input("Введите дату дедлайна для задачи в формате дд-мм-гггг ")
+
+# Проверка
+# print ("Имя пользователя:", username, end=";\n")
+# print("Заголовок заметки:", title, end=";\n")
+# print("Описание заметки:", content, end=";\n")
+# print("Статус заметки:", status, end=";\n")
+# print("Дата создания:", created_date, end=";\n")
+# print("Дедлайн:", issue_date, end=".\n")
