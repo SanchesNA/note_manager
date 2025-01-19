69b187136ef951d4979f592671ac5f59d5fdb45d
SanchesNA <shura.novozhilov.02@mail.ru>
2025-01-19 22:06:17 +1000

    final.py

--- final.py
+++ final.py
@@ -9,8 +9,9 @@ content = input("Введите содержание заметки ")
 created_date = input("Введите дату создания заметки в формате дд-мм-гггг ")
 issue_date = input("Введите дату дедлайна для задачи в формате дд-мм-гггг ")
 
-list_of_titles=[title, under_title1, under_title2]
 
-note=[username, status, content, created_date, issue_date, list_of_titles]
+list_of_titles=[title, under_title1, under_title2] # Создание списка с заголовками
 
-print(note, sep='\n')
+note=[username, status, content, created_date, issue_date, list_of_titles] # Создание списка со всеми данными
+
+print(note, sep='\n') # Вывод списка со всеми данными
